da5e4e873ef57b22bf6ee30fc85a6a4e25aeb590
pendevx <pendevx@gmail.com>
2025-01-11 11:50:53 +1300

    DS-1: unit tests and url rename

---
+++ backend/Music.QueryHandlers.Tests/Accounts/UserRoleCheckerTests.cs
@@ -0,0 +1,63 @@
+﻿using Music.Global.Contracts;
+using Music.QueryHandlers.Accounts;
+using Music.Repository.EF.DatabaseContexts;
+using Music.Repository.EF.Models.Generated;
+using Music.Services.Validators;
+using Music.Tests.Base;
+
+namespace Music.QueryHandlers.Tests.Accounts;
+
+public class UserRoleCheckerTests : BaseTest
+{
+    [Fact]
+    public void ReturnTrueWhen_CurrentUserIsAdmin()
+    {
+        #region setup
+
+        const string username = "admin";
+        var account = DbContext.Accounts.Add(new Account
+        {
+            Username = username,
+            DisplayName = "Admin",
+            SaltedPassword = [],
+        });
+
+        DbContext.SaveChanges();
+
+        var role = DbContext.Roles.Single(r => r.Name == nameof(RoleName.Admin));
+
+        DbContext.AccountRoles.Add(new AccountRole
+        {
+            AccountId = account.Entity.Id,
+            RoleId = role.Id,
+        });
+
+        DbContext.SaveChanges();
+
+        #endregion
+
+        var authContext = new UserRoleCheckerAuthContext(DbContext);
+        var accountHasRoleValidator = new AccountHasRoleValidator(DbContext);
+
+        var handler = new CheckCurrentUserIsAdminHandler(authContext, accountHasRoleValidator);
+
+        var hasAdminRole = handler.Execute();
+
+        Assert.True(hasAdminRole);
+    }
+}
+
+class UserRoleCheckerAuthContext : IAuthContext
+{
+    private readonly MusicContext _dbContext;
+
+    public UserRoleCheckerAuthContext(MusicContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public Account? GetAccount()
+    {
+        return _dbContext.Accounts.FirstOrDefault(acc => acc.Username == "admin");
+    }
+}
---
+++ backend/Music.QueryHandlers/Accounts/CheckCurrentUserIsAdminHandler.cs
@@ -0,0 +1,31 @@
+﻿using Music.Global.Contracts;
+using Music.Repository.EF.Models.Generated;
+using Music.Services.Validators;
+
+namespace Music.QueryHandlers.Accounts;
+
+public class CheckCurrentUserIsAdminHandler : IBaseQueryHandler<bool>
+{
+    private readonly IAuthContext _authContext;
+    private readonly AccountHasRoleValidator _accountHasRoleValidator;
+
+    public CheckCurrentUserIsAdminHandler(
+        IAuthContext authContext,
+        AccountHasRoleValidator accountHasRoleValidator)
+    {
+        _authContext = authContext;
+        _accountHasRoleValidator = accountHasRoleValidator;
+    }
+
+    public bool Execute()
+    {
+        var account = _authContext.GetAccount();
+
+        if (account is null)
+            return false;
+
+        var accountHasRole = _accountHasRoleValidator.Verify(account.Id, RoleName.Admin);
+
+        return accountHasRole;
+    }
+}
