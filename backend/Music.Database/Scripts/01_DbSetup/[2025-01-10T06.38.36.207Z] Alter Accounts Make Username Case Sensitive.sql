1ae42901b1955cb9c1f2ffb571435864635cde7a
pendevx <pendevx@gmail.com>
2025-01-10 19:50:12 +1300

    DS-11: add case-sensitive collation to Username column in Accounts table

---
+++ backend/Music.Database/Scripts/01_DbSetup/[2025-01-10T06.38.36.207Z] Alter Accounts Make Username Case Sensitive.sql
@@ -0,0 +1,9 @@
+﻿IF COL_LENGTH('Accounts', 'Username') IS NOT NULL
+BEGIN
+    ALTER TABLE Accounts DROP CONSTRAINT Accounts_Username_Unique
+    
+    ALTER TABLE Accounts
+    ALTER COLUMN Username VARCHAR(256) COLLATE Latin1_General_CS_AS NOT NULL
+    
+    ALTER TABLE Accounts ADD CONSTRAINT Accounts_Username_Unique UNIQUE(Username)
+END
