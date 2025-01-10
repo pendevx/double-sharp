1eb5b8413f58ebf2ee7454682e44a632d3f69a35
pendevx <pendevx@gmail.com>
2025-01-11 00:08:11 +1300

    DS-1: unit test for registered accounts

--- backend/Music.Tests.Base/BaseTest.cs
+++ backend/Music.Tests.Base/BaseTest.cs
@@ -1,6 +1,29 @@
-﻿namespace Music.Tests.Base;
+﻿using Microsoft.EntityFrameworkCore;
+using Music.Repository.EF.DatabaseContexts;
+using Music.Repository.EF.Models.Generated;
 
-public class BaseTest
+namespace Music.Tests.Base;
+
+public abstract class BaseTest
 {
-    
+    protected readonly MusicContext DbContext;
+
+    protected BaseTest()
+    {
+        var dbOptionsBuilder = new DbContextOptionsBuilder<MusicContext>().UseInMemoryDatabase("MusicThing");
+        
+        DbContext = new MusicContext(dbOptionsBuilder.Options);
+        
+        SetupDbContextWithConstants();
+    }
+
+    private void SetupDbContextWithConstants()
+    {
+        var allRoles = Enum.GetNames<RoleName>()
+            .Select(rn => new Role { Name = rn.ToString() });
+        
+        DbContext.Roles.AddRange(allRoles);
+
+        DbContext.SaveChanges();
+    }
 }
