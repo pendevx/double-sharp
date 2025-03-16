75a0e3525c3b8f61376425e5347c877ea42bf7e8
pendevx <pendevx@gmail.com>
2025-03-16 22:32:00 +1300

    Ds-14: review comments

--- backend/Music.Models.Data/Session.cs
+++ backend/Music.Models.Data/Session.cs
@@ -3,7 +3,6 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Music.Models.Data;
 
-[Index("Token", Name = "UQ__Sessions__1EB4F817BC4B8EF7", IsUnique = true)]
 [Index(nameof(Token), IsUnique = true)]
 public class Session : BaseEntity
 {
