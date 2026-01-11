5473a7c1526badaabb35bf91d846d2fe190d91fb
pendevx <pendevx@gmail.com>
2026-01-11 17:16:44 +1300

    money sum project init

---
+++ moneysum/MoneySum.API/Endpoints/AccountAccessConsent.fs
@@ -0,0 +1,18 @@
+﻿namespace MoneySum.API.Endpoints
+
+open System
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Http
+open Microsoft.AspNetCore.Routing
+
+module AccountAccessConsent =
+
+    let testEndpoint () =
+        Results.Ok "hi"
+
+    testEndpoint.Completed <- System.Action<_>(f)
+
+    let mapAccountAccessConsent (routes: RouteGroupBuilder) =
+        routes.MapGet("/", Func<IResult>(fun () -> testEndpoint())) |> ignore
+
+        routes
---
+++ moneysum/MoneySum.API/Endpoints/Entry.fs
@@ -0,0 +1,9 @@
+﻿namespace MoneySum.API.Endpoints
+
+open AccountAccessConsent
+open Microsoft.AspNetCore.Builder
+
+module Entry =
+
+    let registerEndpoints (app: WebApplication) =
+        app.MapGroup("/account-access-consent")
