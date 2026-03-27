0d5de883c11f5e55bfa7738c73ba541e4eb4e0f1
pendevx <pendevx@gmail.com>
2026-03-28 12:34:52 +1300

    first step for hls

--- double-sharp/backend/Music.Services/SongRequests/SongRequestService.cs
+++ double-sharp/backend/Music.Services/SongRequests/SongRequestService.cs
@@ -17,6 +17,8 @@ public class SongRequestService
     private readonly GetSongPath _getSongPath;
     private readonly SongsRepository _songsRepository;
 
+    private static int[] BitRates => [ 48, 64, 96 ];
+
     public SongRequestService(IAmazonS3 s3Client, GetBucketName getBucketName, GetSongPath getSongPath,
         SongsRepository songsRepository)
     {
@@ -47,9 +49,14 @@ public class SongRequestService
 
         try
         {
-            var mimeType = MimeType.InferFromFileName(path.Data);
-            await using var contents = File.OpenRead(path.Data);
-            await _songsRepository.UploadAsync(newSongId, contents, mimeType);
+            var hlsFiles = await Task.WhenAll(BitRates.Select(ConvertToAac(path.Data)));
+
+            foreach (var result in hlsFiles)
+            {
+                var mimeType = MimeType.InferFromFileName(result);
+                await using var contents = File.OpenRead(result);
+                await _songsRepository.UploadAsync(newSongId, contents, mimeType);
+            }
         }
         finally
         {
@@ -72,4 +79,18 @@ public class SongRequestService
 
         return _s3Client.CopyObjectAsync(request);
     }
+
+    private static Func<int, Task<string>> ConvertToAac(string fileName) => bitrateMbps => Task.Run(() =>
+    {
+        var outputFileName = $"output-{bitrateMbps}.m4a";
+
+        Process.Start(new ProcessStartInfo
+        {
+            FileName = "ffmpeg",
+            Arguments = $"-i {fileName} -c:a aac -b:a {bitrateMbps}k -f hls -hls_time 6 -hls_list_size 0 -hls_segment_filename \"data%03d.ts\" playlist.m3u8",
+            // ffmpeg -i audio.opus -filter_complex "[0:a]asplit=3[a1][a2][a3]" -map "[a1]" -c:a:0 aac -b:a:0 48k -map "[a2]" -c:a:1 aac -b:a:1 64k -map "[a3]" -c:a:2 aac -b:a:2 96k -f hls -hls_time 6 -hls_list_size 0 -hls_playlist_type vod -master_pl_name master.m3u8 -var_stream_map "a:0,name:48k a:1,name:64k a:2,name:96k" -hls_segment_filename "%v/data%03d.m4s" -hls_flags independent_segments -hls_segment_type fmp4 %v/playlist.m3u8
+        });
+
+        return outputFileName;
+    });
 }
